415c0ac98c6c4056cd06ff9dbba1eef2ff3e4e7f
charleseagle <sunqicharles@gmail.com>
2015-11-09 20:03:37 -0700

    SSRS
    SQL Server

---
+++ ClassLibrary1/ClassLibrary1/DateFunctions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class DateFunctions
+    {
+        public static string LongDateFormat(DateTime AnyDate)
+        {
+            string suffix;
+
+            switch (AnyDate.Day)
+            {
+                case 1:
+                case 21:
+                case 31:
+                    suffix = "st";
+                    break;
+                case 2:
+                case 22:
+                    suffix = "nd";
+                    break;
+                case 3:
+                case 23:
+                    suffix = "rd";
+                    break;
+                default:
+                    suffix = "th";
+                    break;
+            }
+            return AnyDate.ToString("dddd d") + suffix +
+                AnyDate.ToString(" MMMM yyyy");
+        }
+    }
+}
---
+++ ClassLibrary1/ClassLibrary1/TestFunctions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    class TestFunctions
+    {
+        public static string Functions()
+        {
+            return "Training";
+        }
+    }
+}
